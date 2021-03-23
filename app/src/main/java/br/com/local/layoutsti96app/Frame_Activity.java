0c9f766185d55c401db488c115fa7d165ea32fb8
Laercio-Note <lndsilva@hotmail.com>
2021-03-23 15:40:16 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/layoutsti96app/Frame_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.layoutsti96app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Frame_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_layout);
+    }
+}
---
+++ app/src/main/java/br/com/local/layoutsti96app/ScrollView_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.layoutsti96app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScrollView_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scroll_view_layout);
+    }
+}
